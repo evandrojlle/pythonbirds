fea7b0ee3ef4284e0b27db4c03b7bd4e1d455fe6
Evandro de Oliveira <evandro.oliveira@univille.br>
2020-05-26 20:37:07 -0300

    Criados atributos de instância: nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, pNome = None, pIdade = 42):
+        self.idade = pIdade
+        self.nome = pNome
+
     def cumprimentar(self):
         return f'OPA {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Juca', 69)
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())
+    print(p.nome)
+    p.nome = 'Evandro'
+    print(p.nome)
+    print(p.idade)
